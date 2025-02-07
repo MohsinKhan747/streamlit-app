1ecd05dbe17838bd8d063bfc7527cf638ae11d55
Mohsin Khan <90383627+MohsinKhan747@users.noreply.github.com>
2025-02-07 23:50:05 +0500

    Add files via upload

---
+++ bmi1.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+def calculate_bmi(weight, height):
+        bmi = weight / (height ** 2)
+        return bmi
+
+def main():
+    st.title("BMI Calculator")
+    st.write("Enter your weight and height to calculate your BMI")
+    
+    weight = st.number_input("Enter your weight (kg):", min_value=1.0,  format="%.2f")
+    height = st.number_input("Enter your height (m):", min_value=0.5, max_value=2.72, format="%.2f")
+    
+    if st.button("Calculate BMI"):
+        if height > 0 and weight > 0:
+            if height >= 2.72:
+                st.error("Height must be less then 2.72")
+                bmi = calculate_bmi(weight, height)
+                st.success(f"Your BMI is: {bmi:.2f}")
+                
+                if bmi < 18.5:
+                    st.info("You are underweight.")
+                elif 18.5 <= bmi < 24.9:
+                    st.success("You have a normal weight.")
+                elif 25 <= bmi < 29.9:
+                    st.warning("You are overweight.")
+                else:
+                    st.error("You are obese.")
+        else:
+            st.error("Weight and height must be greater than zero.")
+
+if __name__ == "__main__":
+    main()
