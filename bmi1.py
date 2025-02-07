2133ab9bd3eb96a66a0ef4dd0ab6729ed915632f
Mohsin Khan <90383627+MohsinKhan747@users.noreply.github.com>
2025-02-07 23:53:12 +0500

    Add files via upload

--- bmi1.py
+++ bmi1.py
@@ -13,19 +13,17 @@ def main():
     
     if st.button("Calculate BMI"):
         if height > 0 and weight > 0:
-            if height >= 2.72:
-                st.error("Height must be less then 2.72")
-                bmi = calculate_bmi(weight, height)
-                st.success(f"Your BMI is: {bmi:.2f}")
-                
-                if bmi < 18.5:
-                    st.info("You are underweight.")
-                elif 18.5 <= bmi < 24.9:
-                    st.success("You have a normal weight.")
-                elif 25 <= bmi < 29.9:
-                    st.warning("You are overweight.")
-                else:
-                    st.error("You are obese.")
+            bmi = calculate_bmi(weight, height)
+            st.success(f"Your BMI is: {bmi:.2f}")
+            
+            if bmi < 18.5:
+                st.info("You are underweight.")
+            elif 18.5 <= bmi < 24.9:
+                st.success("You have a normal weight.")
+            elif 25 <= bmi < 29.9:
+                st.warning("You are overweight.")
+            else:
+                st.error("You are obese.")
         else:
             st.error("Weight and height must be greater than zero.")
 
